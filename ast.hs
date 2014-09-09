0cc18468a93a09307df3b93e0d991964c5232678
Dylan Scott <dylanjgscott@gmail.com>
2014-09-09 13:33:26 +1000

    Added algebraic data type for abstract syntax tree.

--- ast.hs
+++ ast.hs
@@ -1,9 +1,48 @@
 module AST where
 import Prelude
 
-data Expression = Num
-                | Id String
-                | Id Arguments
-                | Expression Op Expression
+-- TODO Use list instead of custom Cons
+-- NOTE This is pretty gross.
+
+data Program = EmptyProgram
+             | Program Function Program
+             deriving Show
+
+data Function = Function Id IdList IdList Statements
+              deriving Show
+
+
+data IdList = EmptyIdList
+            | IdList Id IdList
+            deriving Show
+
+data Statements = EmptyStatements
+                | Statements Statement Statements
                 deriving Show
 
+data Statement = Assign Id Exp
+               | If Id Statements
+               | IfElse Id Statements Statements
+               | Return Id
+               deriving Show
+
+data Exp = ExpNum AST.Num
+         | ExpId Id
+         | ExpFun Id IdList
+         | ExpOp Op Exp Exp
+         deriving Show
+
+data Num = Num Integer
+           deriving Show
+
+data Id = Id String
+        deriving Show
+
+data Op = OpAdd
+        | OpSub
+        | OpMul
+        | OpDiv
+        | OpLT
+        | OpGT
+        | OpEq
+        deriving Show
