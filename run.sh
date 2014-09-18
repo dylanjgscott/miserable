c07f804dfa512fda9538f25f7e2fa2d5cd1aaf04
gobnat <rafael.mazzoldi@gmail.com>
2014-09-18 11:08:42 +1000

    added version for HUnit and fixed a typo

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 #Launcher for the company interpreter
 #Takes 1 or more arguments
 #   - A source file to be interpreted
-#   - zero or more args [Need to check if it has to be at least 1?]
+#   - zero or more args 
 
 numArgs="$#"
 #check for interpreter file
 if [ -x company ]; then
     #check for args
     if [ $numArgs -lt 1 ]; then
-        printf "Wrong number fo argumments. Expected one or more but %s given.\n" $numArgs
+        printf "Wrong number of argumments. Expected one or more but %s given.\n" $numArgs
     else
         #pass args to company and launch
         ./company $@
