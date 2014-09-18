94915c965e61fda06fbf5127324b75d8615741fa
Dylan Scott <dylanjgscott@gmail.com>
2014-09-18 13:55:28 +1000

    hmm I think i might be on to something.

--- Generator.hs
+++ Generator.hs
@@ -14,22 +14,25 @@ genProgram :: Program -> ExeProgram
 genProgram = map genFunction
 
 genFunction :: Function -> ExeFunction
-genFunction (Function id (Args args) _ block) = (id, args, [(0, [["inst"]])])
+genFunction (Function id (Args args) _ block) = (id, args, buildBlocks block 0)
 
+-- need to be able to find blocks within statements
 buildBlocks :: Block -> Integer -> [ExeBlock]
-buildBlocks (Block (s:ss)) n =
+buildBlocks (Block b) n =
     let
-        blocks = genBlockList s (n)
-    in
-        blocks ++ buildBlocks ss (length blocks)
-        
-
-genBlockList :: Statement -> Integer -> [ExeBlock]
-genBlockList s n =
-    let
-        genBlockListHelper (Assign id exp) = (n, genInstruct(Assign id exp))
-        genBlockListHelper (If id block) = 
-        genBlockListHelper (IfElse id bl1 bl2) = 
-        genBlockListHelper (Return id) = 
-    in
-        genBlockListHelper s n
+        buildInstructions :: [Statement] -> [ExeInstruction]
+        buildInstructions [] = []
+        buildInstructions (s:ss) =
+            case s of (Assign id exp) -> ["assign"] : buildInstructions ss
+                      (If id exp) -> ["if"] : buildInstructions ss
+                      (IfElse id exp1 exp2) -> ["ifelse"] : buildInstructions ss
+                      (Return id) -> ["return"] : buildInstructions ss
+        buildMoreBlocks :: [Statement] -> Integer -> [Block]
+        buildMoreBlocks [] _ = []
+        buildMoreBlocks (s:ss) n =
+            case s of (Assign id exp) -> buildMoreBlocks ss n
+                      (If id b) -> buildBlocks b (n+1) : buildMoreBlocks ss (n+2)
+                      (IfElse id b1 b2) -> buildBlocks b1 (n+1) : buildBlocks b2 (n+2): buildMoreBlocks ss (n+3)
+                      (Return id) -> buildMoreBlocks ss n
+    in 
+        (n, buildInstructions b) ++ buildMoreBlocks b (n+1)
