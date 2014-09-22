3588d3ddab24f3e407402f8d234e349a14871a22
Alec Posney <alec.posney@gmail.com>
2014-09-23 04:11:08 +1000

    generation working, except for register naming

--- Generator.hs
+++ Generator.hs
@@ -4,11 +4,11 @@ import Program
 
 type ExeProgram = [ExeFunction]
 type ExeFunction = (ExeId, [ExeId], [ExeBlock])
-type ExeBlock = (Integer, [ExeInstruction])
+type ExeBlock = (ExeBlockId, [ExeInstruction])
 type ExeInstruction = [String]
 type ExeId = String
 type ExeRegister = Integer
-
+type ExeBlockId = Integer
 
 genProgram :: Program -> ExeProgram
 genProgram = map genFunction
@@ -28,14 +28,15 @@ buildBlock [] n = ([], [])
 buildBlock (s:xs) n =
   let
     (instructs, blocks) = case s of
-      (Assign id exp) -> 
-        (instructs ++ buildAssign id (reg), [])
-        where (reg, instructs) = buildExpression exp
-      (Return id) -> (buildReturn id, [])
+      (Assign idr expr) -> 
+        (instructs ++ buildAssign idr reg, [])
+        where (reg, instructs) = buildExpression expr
+      (Return idr) -> (buildReturn idr, [])
       (If cond block) -> 
-        (buildCond cond, buildBlocks block n)
+        ((buildCond cond (fst (head blks)) (-1)), blocks)
+        where blks = buildBlocks block n
       (IfElse cond block1 block2) -> 
-        (buildCond cond, blocks1 ++ blocks2)
+        (buildCond cond (fst (head blocks1)) (fst (head blocks2)), blocks1 ++ blocks2)
         where
           blocks1 = buildBlocks block1 n
           blocks2 = buildBlocks block2 (n + (fromIntegral . length $ blocks1))
@@ -44,7 +45,45 @@ buildBlock (s:xs) n =
   in
     (instructs ++ restInstructs, blocks ++ restBlocks)
 
-buildCond _ = [["cond"]]
-buildAssign _ _= [["assign"]]
-buildExpression _ = (0, [["expression"]])
-buildReturn _ = [["return"]]
+buildExpression :: Exp -> (ExeRegister, [ExeInstruction])
+buildExpression (ExpNum x) = (reg, [["lc", show reg, show x]]) where reg = 0
+buildExpression (ExpId x) = (reg, [["ld", show reg, show x]]) where reg = 0
+buildExpression (ExpFun name args) = 
+  (reg, argInstructs ++ [["call", show reg, show name] ++ (map show argRegs)])
+  where
+    (argRegs, argInstructs) = loadArgs args
+    reg = 0
+buildExpression (ExpOp op expr1 expr2) = 
+  (reg, expr1Instructs ++
+        expr2Instructs ++
+        [[show op,
+          show reg,
+          show expr1reg,
+          show expr2reg]])
+  where
+    (expr1reg, expr1Instructs) = buildExpression expr1
+    (expr2reg, expr2Instructs) = buildExpression expr2
+    reg = 0
+
+loadArgs :: Args -> ([ExeRegister], [ExeInstruction])
+loadArgs (Args args) =
+  let
+    combineArgs (regAcc, instAcc) expression = 
+      (regAcc ++ [reg], instAcc ++ inst)
+      where (reg, inst) = buildExpression (ExpId expression)
+  in
+  foldl combineArgs ([],[]) args
+
+buildAssign :: Id -> ExeRegister -> [ExeInstruction]
+buildAssign idr reg =
+  [["st", show idr, show reg]]
+
+buildCond :: Id -> ExeBlockId -> ExeBlockId -> [ExeInstruction]
+buildCond idr block1 block2 =
+  idInstr ++ [["br", show idreg, show block1, show block2]]
+  where (idreg, idInstr) = buildExpression (ExpId idr)
+
+buildReturn :: Id -> [ExeInstruction]
+buildReturn idr =
+  (instructs ++ [["ret", show reg]])
+  where (reg, instructs) = buildExpression (ExpId idr)
