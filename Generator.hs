d13142f71ce8b9aed754230a4058e0565a1a2632
Alec Posney <alec.posney@gmail.com>
2014-09-23 05:27:59 +1000

    registers are working, but only over each block, might be easier to just to do a find and replace

--- Generator.hs
+++ Generator.hs
@@ -18,69 +18,77 @@ genFunction (Function idr (Args args) _ block) = (idr, args, buildBlocks block 0
 
 -- There are some issues keeping track of block numbers
 -- When calling buildBlocks twice for IfElse the numbering goes out the window.
-buildBlocks :: Block -> Integer -> [ExeBlock]
+buildBlocks :: Block -> ExeBlockId -> [ExeBlock]
 buildBlocks (Block blk) n = 
   (n, instructs) : blocks
-  where (instructs, blocks) = buildBlock blk n
+  where (instructs, blocks) = buildBlock blk n 1
 
-buildBlock :: [Statement] -> Integer -> ([ExeInstruction], [ExeBlock])
-buildBlock [] n = ([], [])
-buildBlock (s:xs) n =
+buildBlock :: [Statement] -> ExeBlockId -> ExeRegister -> ([ExeInstruction], [ExeBlock])
+buildBlock [] n reg = ([], [])
+buildBlock (s:xs) n reg =
   let
-    (instructs, blocks) = case s of
+    (nextReg, instructs, blocks) = case s of
       (Assign idr expr) -> 
-        (instructs ++ buildAssign idr reg, [])
-        where (reg, instructs) = buildExpression expr
-      (Return idr) -> (buildReturn idr, [])
+        (retReg + 1, instructs ++ asgnInstr, [])
+        where
+          (nextReg, instructs) = buildExpression expr reg
+          (retReg, asgnInstr) = buildAssign idr nextReg
+      (Return idr) -> (retReg + 1, retInstr, [])
+        where (retReg, retInstr) = buildReturn idr reg
       (IfElse cond block1 block2) -> 
-        (buildCond cond (fst (head blocks1)) (fst (head blocks2)), blocks1 ++ blocks2)
+        (retReg + 1, condInstr, blocks1 ++ blocks2)
         where
           blocks1 = buildBlocks block1 n
           blocks2 = buildBlocks block2 (n + (fromIntegral . length $ blocks1))
+          (retReg, condInstr) = buildCond cond (fst (head blocks1)) (fst (head blocks2)) reg
 
-    (restInstructs, restBlocks) = buildBlock xs (n + 1 + (fromIntegral . length $ blocks))
+    (restInstructs, restBlocks) = buildBlock xs (n + 1 + (fromIntegral . length $ blocks)) nextReg
   in
     (instructs ++ restInstructs, blocks ++ restBlocks)
 
-buildExpression :: Exp -> (ExeRegister, [ExeInstruction])
-buildExpression (ExpNum x) = (reg, [["lc", show reg, show x]]) where reg = 0
-buildExpression (ExpId x) = (reg, [["ld", show reg, x]]) where reg = 0
-buildExpression (ExpFun name args) = 
-  (reg, argInstructs ++ [["call", show reg, name] ++ (map show argRegs)])
+buildExpression :: Exp -> ExeRegister -> (ExeRegister, [ExeInstruction])
+buildExpression (ExpNum x) reg = (reg, [["lc", show reg, show x]])
+buildExpression (ExpId x) reg = (reg, [["ld", show reg, x]])
+buildExpression (ExpFun name args) reg = 
+  ((retReg), argInstructs ++ [["call", show (retReg), name] ++ (map show argRegs)])
   where
-    (argRegs, argInstructs) = loadArgs args
-    reg = 0
-buildExpression (ExpOp op expr1 expr2) = 
-  (reg, expr1Instructs ++
+    (argRegs, argInstructs) = loadArgs args reg
+    retReg = (last argRegs) + 1 
+buildExpression (ExpOp op expr1 expr2) reg = 
+  (retReg, expr1Instructs ++
         expr2Instructs ++
         [[show op,
-          show reg,
+          show retReg,
           show expr1reg,
           show expr2reg]])
   where
-    (expr1reg, expr1Instructs) = buildExpression expr1
-    (expr2reg, expr2Instructs) = buildExpression expr2
-    reg = 0
+    (expr1reg, expr1Instructs) = buildExpression expr1 reg
+    (expr2reg, expr2Instructs) = buildExpression expr2 (expr1reg + 1)
+    retReg = expr2reg + 1
 
-loadArgs :: Args -> ([ExeRegister], [ExeInstruction])
-loadArgs (Args args) =
+loadArgs :: Args -> ExeRegister -> ([ExeRegister], [ExeInstruction])
+loadArgs (Args args) reg =
   let
     combineArgs (regAcc, instAcc) expression = 
       (regAcc ++ [reg], instAcc ++ inst)
-      where (reg, inst) = buildExpression (ExpId expression)
+      where
+        reg = ((last regAcc) + 1)
+        (nextReg, inst) = buildExpression (ExpId expression) reg
+
+    (argRegs, argInstr) = foldl combineArgs ([reg],[]) args
   in
-  foldl combineArgs ([],[]) args
+    (tail argRegs, argInstr)
 
-buildAssign :: Id -> ExeRegister -> [ExeInstruction]
+buildAssign :: Id -> ExeRegister -> (ExeRegister, [ExeInstruction])
 buildAssign idr reg =
-  [["st", idr, show reg]]
+  (reg, [["st", idr, show reg]])
 
-buildCond :: Id -> ExeBlockId -> ExeBlockId -> [ExeInstruction]
-buildCond idr block1 block2 =
-  idInstr ++ [["br", show idreg, show block1, show block2]]
-  where (idreg, idInstr) = buildExpression (ExpId idr)
+buildCond :: Id -> ExeBlockId -> ExeBlockId -> ExeRegister -> (ExeRegister, [ExeInstruction])
+buildCond idr block1 block2 reg =
+  (retReg, idInstr ++ [["br", show retReg, show block1, show block2]])
+  where (retReg, idInstr) = buildExpression (ExpId idr) reg
 
-buildReturn :: Id -> [ExeInstruction]
-buildReturn idr =
-  (instructs ++ [["ret", show reg]])
-  where (reg, instructs) = buildExpression (ExpId idr)
+buildReturn :: Id -> ExeRegister -> (ExeRegister, [ExeInstruction])
+buildReturn idr reg =
+  (retReg, (instructs ++ [["ret", show retReg]]))
+  where (retReg, instructs) = buildExpression (ExpId idr) reg
