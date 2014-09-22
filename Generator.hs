d5fedb2f4b85a01a080aadfcf04a140ea1b0f03b
Alec Posney <alec.posney@gmail.com>
2014-09-22 15:18:50 +1000

    alecs generator, partially working

--- Generator.hs
+++ Generator.hs
@@ -14,30 +14,36 @@ genProgram :: Program -> ExeProgram
 genProgram = map genFunction
 
 genFunction :: Function -> ExeFunction
-genFunction (Function id (Args args) _ block) = (id, args, buildBlocks block 0)
+genFunction (Function id (Args args) _ block) = (id, args, (0, instructs) : blocks)
+  where (instructs, blocks) = buildBlocks block 0
 
 -- There are some issues keeping track of block numbers
 -- When calling buildBlocks twice for IfElse the numbering goes out the window.
-buildBlocks :: Block -> Integer -> [ExeBlock]
-buildBlocks (Block s:xs) n =
-    let
-        buildBlock :: Statment -> ([ExeInstruction], [ExeBlock])
-        buildBlock (Assign id exp) = 
-          (instructs ++ buildAssign id (reg), [])
-          where (reg, instructs) = buildExpression exp
-
-        buildBlock (Return id) = (buildReturn id, [])
-
-        buildBlock (If cond block) = 
-          (buildCond + instructs, blocks)
-          where (instructs, blocks) = buildBlocks block n
-
-        buildBlock (IfElse cond block1 block2) = 
-          (buildCond cond + instructs1 + instructs2, blocks1 + block2)
-          where
-            (instructs1, blocks1) = buildBlocks block1 n
-            (instructs2, blocks2) = buildBlocks block2 n + length(blocks1)
-    in 
-        (n, wholeBlock) : blocks ++ buildBlocks xs (n + 1 + length(blocks))
+buildBlocks :: Block -> Integer -> ([ExeInstruction], [ExeBlock])
+buildBlocks (Block (s:xs)) n =
+  let 
+    (wholeBlock, blocks) = case s of
+      (Assign id exp) -> 
+        (instructs ++ buildAssign id (reg), [])
+        where (reg, instructs) = buildExpression exp
+      (Return id) -> (buildReturn id, [])
+      (If cond block) -> 
+        (buildCond cond ++ instructs, blocks)
+        where (instructs, blocks) = buildBlocks block n
+      (IfElse cond block1 block2) -> 
+        (buildCond cond ++ instructs1 ++ instructs2, blocks1 ++ blocks2)
         where
-          wholeBlock, blocks = buildBlock s
+          (instructs1, blocks1) = buildBlocks block1 n
+          (instructs2, blocks2) = buildBlocks block2 (n + (fromIntegral . length $ blocks1))
+
+    (restBlock, restBlocks) = case xs of 
+       [] -> ([], [])
+       xs' -> buildBlocks (Block xs') (n + 1 + (fromIntegral . length $ blocks))
+  in 
+    (wholeBlock ++ restBlock,  blocks ++ restBlocks) 
+          
+
+buildCond _ = [["cond"]]
+buildAssign _ _= [["assign"]]
+buildExpression _ = (0, [["expression"]])
+buildReturn _ = [["return"]]
