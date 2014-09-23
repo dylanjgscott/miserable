90e237989322810d5dd013414f7cc0cb25315463
Dylan Scott <dylanjgscott@gmail.com>
2014-09-23 19:59:26 +1000

    Removed some of the redundant where statements.

--- Interpreter.hs
+++ Interpreter.hs
@@ -14,36 +14,38 @@ runProgram p s = runFunction p "main" s
     where
         runFunction :: AsmProgram -> AsmId -> MachineState -> Integer
         runFunction p id s = runFunctionHelper (findFunction p id) s
+
+        runFunctionHelper :: AsmFunction -> MachineState -> Integer
+        runFunctionHelper (AsmFunction _ _ blocks) s = runBlock blocks 0 s
             where
-                runFunctionHelper :: AsmFunction -> MachineState -> Integer
-                runFunctionHelper (AsmFunction _ _ blocks) s = runBlock blocks 0 s
-                    where
-                    runBlock blocks num state = runBlockHelper (findBlock blocks num) state
-                        where
-                            runBlockHelper :: AsmBlock -> MachineState -> Integer
-                            runBlockHelper (AsmBlock _ instructions) s = runInstructions instructions s
-                                where
-                                    runInstructions :: [AsmInstruction] -> MachineState -> Integer
-                                    runInstructions (i:is) s =
-                                        case i of
-                                            (AsmLc reg num) -> runInstructions is (setReg reg num s)
-                                            (AsmLd reg id) -> runInstructions is (setReg reg (getMem id s) s)
-                                            (AsmSt id reg) -> runInstructions is (setMem id (getReg reg s) s)
-                                            (AsmAdd reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) + (getReg reg3 s)) s)
-                                            (AsmSub reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) - (getReg reg3 s)) s)
-                                            (AsmMul reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) * (getReg reg3 s)) s)
-                                            (AsmDiv reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) `div` (getReg reg3 s)) s)
-                                            (AsmLt reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) < (getReg reg3 s)) then 1 else 0) s)
-                                            (AsmGt reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) > (getReg reg3 s)) then 1 else 0) s)
-                                            (AsmEq reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) == (getReg reg3 s)) then 1 else 0) s)
-                                            (AsmBr reg b1 b2) -> if reg == 0 then runBlock blocks b2 s else runBlock blocks b1 s
-                                            (AsmRet r) -> getReg r s
-                                            (AsmCall reg id rs) ->
-                                                let func = findFunction p id
-                                                    funcArgs = getFunctionArgs func
-                                                    newState = (zip funcArgs (map (\x -> getReg x s) rs), [])
-                                                    funcResult = runFunction p id newState
-                                                in runInstructions is (setReg reg funcResult s)
+                runBlock blocks num state = runBlockHelper (findBlock blocks num) state
+                runBlockHelper :: AsmBlock -> MachineState -> Integer
+                runBlockHelper (AsmBlock _ instructions) s = runInstructions instructions s
+
+                runInstructions :: [AsmInstruction] -> MachineState -> Integer
+                runInstructions (i:is) s =
+                    case i of
+                        (AsmLc reg num) -> runInstructions is (setReg reg num s)
+                        (AsmLd reg id) -> runInstructions is (setReg reg (getMem id s) s)
+                        (AsmSt id reg) -> runInstructions is (setMem id (getReg reg s) s)
+                        (AsmAdd reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) + (getReg reg3 s)) s)
+                        (AsmSub reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) - (getReg reg3 s)) s)
+                        (AsmMul reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) * (getReg reg3 s)) s)
+                        (AsmDiv reg1 reg2 reg3) -> runInstructions is (setReg reg1 ((getReg reg2 s) `div` (getReg reg3 s)) s)
+                        (AsmLt reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) < (getReg reg3 s)) then 1 else 0) s)
+                        (AsmGt reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) > (getReg reg3 s)) then 1 else 0) s)
+                        (AsmEq reg1 reg2 reg3) -> runInstructions is (setReg reg1 (if ((getReg reg2 s) == (getReg reg3 s)) then 1 else 0) s)
+                        (AsmBr reg b1 b2) -> if reg == 0 then runBlock blocks b2 s else runBlock blocks b1 s
+                        (AsmRet r) -> getReg r s
+                        -- I think there is a bug in the code below.
+                        (AsmCall reg id rs) ->
+                            let
+                                func = findFunction p id
+                                funcArgs = getFunctionArgs func
+                                newState = (zip funcArgs (map (\x -> getReg x s) rs), [])
+                                funcResult = runFunction p id newState
+                            in
+                                runInstructions is (setReg reg funcResult s)
 
 findFunction :: [AsmFunction] -> AsmId -> AsmFunction
 findFunction [] _ = error "No main program defined."
