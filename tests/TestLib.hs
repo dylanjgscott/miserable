93fed9cb9c16f86a4b6b3908efa5bd11ef4f8479
gobnat <rafael.mazzoldi@gmail.com>
2014-09-25 00:39:13 +1000

    fixed some spelling errors

--- tests/TestLib.hs
+++ tests/TestLib.hs
@@ -39,9 +39,8 @@ assertException ex action =
 
 #if __GLASGOW_HASKELL__ < 708
 
--- There seems to have been a change in ghc 7.8 that breaks my code
--- if the following like is included, however the instance decleration is needed
--- in < 7.8 so I have resorted to preprocessor code in case this gets compiled in ghc 7.8+
+-- There seems to have been a change in ghc 7.8 that breaks my code if the following line is included. 
+-- However, the instance declaration is needed in < ghc 7.8 so I have resorted to preprocessor code in case this gets compiled in ghc 7.8+
 
 
 -- Need an instance decleration so I can pass a String in for the Error type
