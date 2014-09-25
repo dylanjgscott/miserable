bbf925c205ea16241e026b400102a2d8d58bdcee
Alec Posney <alec.posney@gmail.com>
2014-09-25 16:43:38 +1000

    cleaned up comments

--- src/misery/Generator.hs
+++ src/misery/Generator.hs
@@ -10,8 +10,12 @@ type ExeInstruction = [String]
 type ExeId = String
 type ExeBlockId = Int
 type ExeRegister = Int
---               Start         Return          Next Block  Next Reg
-type LazyBlock = ExeBlockId -> ExeRegister -> (ExeBlockId, ExeRegister, [ExeBlock])
+
+
+-- input         Start Id      Return Id
+type LazyBlock = ExeBlockId -> ExeRegister -> 
+-- return        Next Block  Next Reg     Blocks
+                (ExeBlockId, ExeRegister, [ExeBlock])
 
 -- maps generated functions from the ast function list
 genProgram :: Program -> ExeProgram
@@ -27,8 +31,18 @@ genFunction (Function idr (Args args) _ block) =
 buildBlocks :: Block -> [ExeBlock]
 buildBlocks block =
   let
+  -- Set the value of register zero to 0
+  -- not necessary for the spec, but should
+  -- protect badly written interpreters.
   zeroRegister = ["lc", showReg 0, show 0]
   zeroBlock = (0, zeroRegister : (buildJump 1))
+
+  -- Build this block and all children blocks
+  -- Currently the return jump at the end of a 
+  -- root block is set to -1, this is not in the
+  -- grammar but if this jump is hit, the interpreter
+  -- should crash anyway, as this will only occur
+  -- if a functions doesn't return
   (_, _, blocks) = buildBlocks' block 1 (-1) 2 1 
   in 
     zeroBlock : blocks
@@ -39,11 +53,19 @@ buildBlocks' :: Block -> ExeBlockId -> ExeBlockId  -> LazyBlock
 buildBlocks' (Block statements) rootBlockId returnBlockId  start reg =
   let
     -- Build root block
+    -- A single block with only a jump
+    -- It is the entry point from 
+    -- a branch
     rootBlock' = buildJump start
-    rootBlock   = (rootBlockId,  rootBlock'  ) -- build actual start block
     nextBlockId = start
+    rootBlock   = (rootBlockId,  rootBlock'  ) -- build actual start block
 
     -- Current block aggrigator
+    -- Build the actual instructions with each 
+    -- statement being given a new block.
+    -- It also builds a list of partially constructed
+    -- blocks which are direct children of this block
+    -- i.e. they are a block in an if else statement
     combine :: (ExeBlockId, ExeRegister, [ExeBlock], [LazyBlock]) -> Statement -> (ExeBlockId, ExeRegister, [ExeBlock], [LazyBlock])
     combine (startBlockId, startReg, blockAcc, superBlockAcc) statement =
       (accBlockId, accRegister, blockAcc ++ [block], superBlockAcc ++ superBlocks)
@@ -51,6 +73,9 @@ buildBlocks' (Block statements) rootBlockId returnBlockId  start reg =
         (accBlockId, accRegister, block, superBlocks) = buildBlock statement startBlockId startReg
 
     -- LazyBlock aggrigator
+    -- Recursively applies the partial blocks
+    -- Constructing all children in a pre-order
+    -- traversal
     apply :: (ExeBlockId, ExeRegister, [ExeBlock]) -> LazyBlock -> (ExeBlockId, ExeRegister, [ExeBlock])
     apply (startBlockId, startReg, blockAcc) superBlock =
       (accBlockId, accRegister, blockAcc ++ blocks)
@@ -64,7 +89,7 @@ buildBlocks' (Block statements) rootBlockId returnBlockId  start reg =
     returnBlock' = buildJump returnBlockId
     returnBlock = (finalBlockId', returnBlock') -- build actual return block
 
-    -- Build final meta-block
+    -- Build final block list
     block = rootBlock : blocks ++ [returnBlock]
 
     -- Evaluate lazy blocks
@@ -76,7 +101,9 @@ buildBlocks' (Block statements) rootBlockId returnBlockId  start reg =
 buildBlock :: Statement -> ExeBlockId -> ExeRegister -> (ExeBlockId, ExeRegister, ExeBlock, [LazyBlock])
 buildBlock statement blockId register =
   let
+    -- Match each statment and build all its instructions
     (nextBlockId, nextReg, instructs, blocks) = case statement of
+      -- Assignment Statement
       (Assign idr expr) -> 
         (ret, retReg + 1, instructs ++ asgnInstr, [])
         where
@@ -84,12 +111,14 @@ buildBlock statement blockId register =
           (nextReg, instructs) = buildExpression expr register
           (retReg, asgnInstr) = buildAssign idr nextReg
 
+      -- Return Statement
       (Return idr) -> 
         (ret, retReg + 1, retInstr, [])
         where
           ret = blockId + 1
           (retReg, retInstr) = buildReturn idr register
         
+      -- IfElse Statement
       (IfElse cond block1 block2) -> 
         (ret, retReg + 1, condInstr, [blocks1, blocks2])
         where
@@ -97,19 +126,28 @@ buildBlock statement blockId register =
           ref1 = blockId + 1 -- first branch of ifelse
           ref2 = blockId + 2 -- second branch of iflse
           ret  = blockId + 3 -- return block after iflse
-          blocks1 = buildBlocks' block1 ref1 ret
+          -- Partially construct childdren
+          blocks1 = buildBlocks' block1 ref1 ret 
           blocks2 = buildBlocks' block2 ref2 ret
+          -- Build condition instructions
           (retReg, condInstr) = buildCond cond ref1 ref2 register
 
+    -- Add jump instruction to the end of the statement
+    -- to point to the next block in the block list
     jump = buildJump nextBlockId
     block = (blockId, instructs ++ jump)
     in
       (nextBlockId, nextReg, block, blocks)
 
 
+-- Helper function to build psudo jump instruction
+-- consists of a branch instruction where
+-- both outcomes point to the same location
 buildJump :: ExeBlockId -> [ExeInstruction]
 buildJump target = [["br", showReg 0, show target, show target]]
 
+-- Recursively build all intructions required for a givn expression
+-- Uses a preorder traversion of the expression tree
 buildExpression :: Exp -> ExeRegister -> (ExeRegister, [ExeInstruction])
 -- Base cases
 buildExpression (ExpNum x) reg = (reg, [["lc", showReg reg, show x]])
